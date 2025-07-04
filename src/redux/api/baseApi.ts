84fa79cb4e3ad69ed6b3329a6feaa10b58d75eba
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-07-04 12:14:02 +0600

    add

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_BASE_URL}/api` }),
   tagTypes: ["book"],
   endpoints: (builder) => ({
     getBooks: builder.query({
